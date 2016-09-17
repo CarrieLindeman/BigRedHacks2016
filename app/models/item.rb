1697e03bbaf0f09a5ae6b63880723ea123413423
Evan Sobkowicz <evansobkowicz@gmail.com>
2016-09-17 15:30:33 -0400

    fixes

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,24 +1,22 @@
 class Item < ActiveRecord::Base
-<<<<<<< HEAD
     include ActionView::Helpers::DateHelper
-    
+
     def days_until_date
         distance_of_time_in_words(expiration, Date.today)
     end
-    
+
     def color_of_days
     	rgb = [0, 0, 0]
     	if (expiration > Date.today)
     		if (expiration - Date.today > 14)
     			rgb[1] = 255
-    		else if (expiration - Date.today < 1)
+    		elsif (expiration - Date.today < 1)
     			rgb[0] = 255
     		else
     			rgb[1] = ((expiration - Date.today).to_i / 14) * 255
     			rgb[0] = ((14 - (expiration - Date.today).to_i) / 14) * 255
     		end
     	end
-    end 
-=======
->>>>>>> parent of e995e89... Added missing files
+    end
+
 end
