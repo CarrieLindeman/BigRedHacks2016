bb8df4c2761749ee8aebf2596b379b5a1815f945
Evan Sobkowicz <evansobkowicz@gmail.com>
2016-09-17 17:48:31 -0400

    fix color calc

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,21 +5,34 @@ class Item < ActiveRecord::Base
         distance_of_time_in_words(expiration, Date.today)
     end
 
-    def color_of_days
-    	rgb = [0, 0, 0]
-    	if (expiration > Date.today)
-    		if (expiration - Date.today > 14)
-    			rgb[1] = 255
-    		elsif (expiration - Date.today < 1)
-    			rgb[0] = 255
-    		else
-    			rgb[1] = ((expiration - Date.today).to_i / 14) * 255
-    			rgb[0] = ((14 - (expiration - Date.today).to_i) / 14) * 255
-    		end
-      else
-        rgb[0] = 255
-    	end
-      rgb
+    def color
+       minColor = [0,0,255]
+       maxColor = [255,0,0]
+
+       days = (expiration - Date.today).to_i
+       score = calcScorePerc(days)
+
+       r = calculateColor(maxColor[0], minColor[0], score)
+       g = calculateColor(maxColor[1], minColor[1], score)
+       b = calculateColor(maxColor[2], minColor[2], score)
+
+       [r, g, b]
+    end
+
+    def calcScorePerc(score)
+      normalized_score = (score < 0) ? 14 : score
+      max = 14
+      min = 0
+      range = max - min
+      ((normalized_score - min) * 100) / range
+    end
+
+
+    def calculateColor(max_color, min_color, day_percent)
+       color_range = max_color - min_color
+       score = (color_range < 0) ? (100 - day_percent) : day_percent
+       additional_value = (color_range < 0) ? max_color : min_color
+       (score * color_range.abs) / 100 + additional_value
     end
 
 end
