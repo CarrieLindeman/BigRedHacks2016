5ca33820df74bcd78e7b908516790231c998c7d9
Evan Sobkowicz <evansobkowicz@gmail.com>
2016-09-17 00:17:06 -0400

    config

--- config/deploy.rb
+++ config/deploy.rb
@@ -34,7 +34,7 @@ task :environment do
   # invoke :'rbenv:load'
 
   # For those using RVM, use this to load an RVM version@gemset.
-  invoke :'rvm:use[ruby-2.1.3@default]'
+  invoke :'rvm:use[ruby-2.0.0-p353@default]'
 end
 
 # Put any custom mkdir's in here for when `mina setup` is ran.
